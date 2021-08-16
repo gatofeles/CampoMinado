11a1db29cc746df442b79b14794f98f0a2ac1cea
gatofeles <alexofeles@gmail.com>
2021-08-15 21:54:56 -0300

    distribuicao de bomba

--- js/gerarCampo.js
+++ js/gerarCampo.js
@@ -1,7 +1,9 @@
 function gerarCampo(){
-    let bomb = document.getElementById("bomb").value;
+    const bomb = document.getElementById("bomb").value;
     let linhas = document.getElementById("linhas").value;
     let colunas = document.getElementById("colunas").value;
+    let matrizCampo = [];
+    let bombCounter = 0;
 
     // console.log("bomb"+bomb);
     // console.log("linhas"+linhas);
@@ -21,16 +23,35 @@ function gerarCampo(){
         
         for(let i = 0; i< linhas; i++){
             const novaLinha = document.createElement("tr");
+            matrizCampo[i] = new Array(colunas);
+
             for(let j = 0; j < colunas; j++){
                 const novaColuna = document.createElement("td");
                 novaColuna.classList.add("area");
                 novaLinha.appendChild(novaColuna);
+                matrizCampo[i][j] = novaColuna;
             }
             campo.appendChild(novaLinha);
             
         }
 
+        while(bomb>bombCounter){
+
+            for(let i = 0; i< linhas; i++){
+                for(let j = 0; j < colunas; j++){
+                   const sorteio = Math.floor((Math.random()*100));
+                   if(sorteio<5 && bombCounter<bomb && matrizCampo[i][j].className !== "areaBomba"){
+                       matrizCampo[i][j].className = "areaBomba";
+                       matrizCampo[i][j].innerHTML = '<img id = "bombaArea" src="../images/bombear.png" alt="bomba">';
+                       bombCounter++;
+
+                   }
+                }    
+            }
+        }
+
     }
 
 }
+
 

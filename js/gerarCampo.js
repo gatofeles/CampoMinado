4fb05d3dcae0ff18d7ccf98dc7dcd3ac3cfb2b70
gatofeles <alexofeles@gmail.com>
2021-08-22 11:36:26 -0300

    logica geral de bombas

--- js/gerarCampo.js
+++ js/gerarCampo.js
@@ -17,6 +17,10 @@ function gerarCampo(){
         alert("Valores precisam ser positivos.");
     }
 
+    else if(bomb > (colunas*linhas)-1){
+        alert("Quantidade de bombas excede o numero de celulas.");
+    }
+
     else{
         const campo = document.getElementById("table");
         campo.innerHTML = "";
@@ -28,6 +32,7 @@ function gerarCampo(){
             for(let j = 0; j < colunas; j++){
                 const novaColuna = document.createElement("td");
                 novaColuna.classList.add("area");
+                novaColuna.setAttribute("onclick", "revelarCampos()");
                 novaLinha.appendChild(novaColuna);
                 matrizCampo[i][j] = novaColuna;
             }
@@ -42,7 +47,7 @@ function gerarCampo(){
                    const sorteio = Math.floor((Math.random()*100));
                    if(sorteio<5 && bombCounter<bomb && matrizCampo[i][j].className !== "areaBomba"){
                        matrizCampo[i][j].className = "areaBomba";
-                       matrizCampo[i][j].innerHTML = '<img id = "bombaArea" src="../images/bombear.png" alt="bomba">';
+                       matrizCampo[i][j].innerHTML = '<img class = "bombaHide" src="../images/bombear.png" alt="bomba">';
                        bombCounter++;
 
                    }
@@ -54,4 +59,83 @@ function gerarCampo(){
 
 }
 
+function revelarCampos(){
+   
+   const celula = event.target;
+   let matrizCampo = [];
+   let index = [];
+   const campo = document.getElementById("table").children;
+
+   if(celula.className.toString() === "areaBomba"){
+    celula.children[0].className = "bombaArea";
+    return;
+    }
+
+   for(let i = 0; i < campo.length; i++){
+       matrizCampo[i] = new Array(campo[i].children.length);
+       for(let j = 0; j < campo[i].children.length; j++){
+            matrizCampo[i][j] = campo[i].children[j];
+            if(matrizCampo[i][j] === celula){
+                index.push(i,j);
+            }
+       }
+    }
+     
+    revelarRecursivo(matrizCampo, index);
+
+
+}
+
+function revelarRecursivo(matriz, index){
+    console.log("entrei: "+ index);
+    if(matriz[index[0]][index[1]].className.toString() === "areaBomba"){
+        return;
+    }
+    
+    else{
+        let bombasPerto = 0;
+        console.log("matrizTipoBefore: " + (matriz[index[0]][index[1]].className.toString() === "area"));
+        matriz[index[0]][index[1]].className = "areaRevel";
+        console.log("matrizTipo: " + matriz[index[0]][index[1]].className);
+
+        for(let i = -1; i <= 1; i++){
+            for(let j = -1; j <= 1; j++){
+                
+                if(index[0]+i >=0 && index[1]+j >= 0 && index[0]+i < matriz.length && index[1]+j < matriz[index[0]].length){
+                    console.log(index[0]+i);
+                    console.log(index[1]+j);
+                    console.log("-----");
+                    //await sleep(50);
+
+                    if(matriz[index[0]+i][index[1]+j].className.toString() === "areaBomba"){
+                        bombasPerto++;
+                    }
+                }
+            }
+        }
+
+        if(bombasPerto)
+            matriz[index[0]][index[1]].innerHTML = bombasPerto;
+
+        for(let i = -1; i <= 1; i++){
+            for(let j = -1; j <= 1; j++){
+                if(index[0]+i >=0 && index[1]+j >= 0 && index[0]+i < matriz.length && index[1]+j < matriz[index[0]].length){
+                    if(!bombasPerto && matriz[index[0]+i][index[1]+j].className.toString() !== "areaRevel" ){
+                        revelarRecursivo(matriz, [index[0]+i, index[1]+j]);
+                }
+                
+                }
+            }
+        }
+
+        
+        
+
+    }
+}
+
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
 

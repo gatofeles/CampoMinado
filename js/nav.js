ddc82ad2f3cc242b65ab919bf31371f85c039426
Wagner Martins Jr <64937633+gatofeles@users.noreply.github.com>
2021-09-19 11:21:26 -0300

    Revert "Revert "Rlima ranking""

---
+++ js/nav.js
@@ -0,0 +1,19 @@
+function goRegister(){
+	window.location.href = './register.html';
+}
+
+function goGame(){
+	window.location.href = './game.html';
+}
+
+function goPersonalRanking(){
+	window.location.href = './personal_ranking.html';
+}
+
+function goGlobalRanking(){
+	window.location.href = './global_ranking.html';
+}
+
+function logout(){
+	window.location.href = './index.html';
+}
